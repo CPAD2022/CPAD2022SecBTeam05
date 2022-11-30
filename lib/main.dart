b3ed91fe890c9fee42fbdc484e8dad8a2672bdab
H Deepak D Bhat <h.deepak.d.bhat@sap.com>
2022-11-30 19:40:28 +0530

    matching figma to an extent

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@ import 'package:demoapp/socialSignin.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:go_router/go_router.dart';
 import 'firebase_options.dart';
-
 
 import 'package:demoapp/spalshScreen/splashScreen.dart';
 import 'package:demoapp/login.dart';
-
+import 'package:demoapp/homePage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,12 +15,42 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: 'splash',
-    routes: {
-      'splash':(context) => const SplashScreen(),
-      'login': (context) => MySocialLogin(),
-    },
-  ));
+  // runApp(MaterialApp(
+  //   debugShowCheckedModeBanner: false,
+  //   initialRoute: 'splash',
+  //   routes: {
+  //     'splash':(context) => const SplashScreen(),
+  //     'login': (context) => MySocialLogin(),
+  //   },
+  // ));
+
+  final GoRouter _router = GoRouter(
+    routes: <RouteBase>[
+      GoRoute(
+        path: '/',
+        builder: (context, state) => SplashScreen(),
+        routes: <RouteBase>[
+          GoRoute(
+            path: 'login',
+            builder: (context, state) => MyLogin(),
+          ),
+          GoRoute(
+            path: 'signup',
+            builder: (context, state) => MySocialLogin(),
+          ),
+          GoRoute(
+            path: 'home',
+            builder: (context, state) => MyHomePage(),
+          )
+        ],
+      ),
+    ],
+  );
+
+  runApp(
+    MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: _router,
+    ),
+  );
 }
