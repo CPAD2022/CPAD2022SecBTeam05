4b2256dde6c7bcfbe0cddddaf71fd22aeaa92e10
whoareyoukid <roshroshnik@gmail.com>
2022-12-03 00:21:43 +0530

    trying out cf

--- MLmodel/app.py
+++ MLmodel/app.py
@@ -1,22 +1,37 @@
-import firebase_admin
-from firebase_admin import ml
-from firebase_admin import credentials
-import tensorflow as tf
+import os
+import numpy as np
+import pandas as pd
+import os
+from glob import glob
+from PIL import Image
 
-firebase_admin.initialize_app(
-    credentials.Certificate('C:\\Users\\I526744\\Documents\\BITS\\SEM 3\\CPAD\\Assignment\\CPAD2022SecBTeam05\\MLmodel\\application-name-91fa1-firebase-adminsdk-c5160-6254dd99f4.json'),
-    options = {
-        'storageBucket': 'application-name-91fa1.appspot.com'
-})
+from flask import Flask, request, jsonify
 
-source = ml.TFLiteGCSModelSource.from_tflite_model_file('C:\\Users\\I526744\\Documents\\BITS\\SEM 3\\CPAD\\Assignment\\CPAD2022SecBTeam05\\MLmodel\\model_skin.tflite')
+app = Flask(__name__)
 
-tflite_format = ml.TFLiteFormat(model_source = source)
-model = ml.Model(
-    display_name = "test_model",
-    tags = ["examples"],
-    model_format = tflite_format
-)
+@app.route('/') 
+def hello_world():
+    return 'Hello, World!'
 
-new_model = ml.create_model(model)
-ml.publish_model(new_model.model_id)
+import pickle
+import wget
+import requests # request img from web
+import shutil # save img locally
+import urllib.request
+
+@app.route('/predictcancer/<path:imgpath>')
+def predict_cancer(imgpath):
+    imgpath = requests.get(imgpath, stream = True)
+    with open(imgpath,'wb') as f:
+        shutil.copyfileobj(imgpath.raw, f)
+    pickled_model = pickle.load(open('model.pkl', 'rb'))   
+    test_image = np.asarray(Image.open(imgpath).resize((224,224)).convert("RGB"))
+    test_image = test_image/255.
+    test_image = np.array([test_image])
+    res = pickled_model.predict(test_image.reshape(test_image.shape[0],-1))
+    if res == 0:
+        res = "Negative"
+    else:
+        res = "Positive"
+    return jsonify({'res':res,'file':imgpath})
+
